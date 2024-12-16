f8d1430701e1522f08b3a29b053a8614b96f6078
Arthur Jenck <arthurjenckdev@gmail.com>
2024-12-16 11:50:46 +0100

    feat: added server action to client component

--- src/app/form/page.jsx
+++ src/app/form/page.jsx
@@ -1,18 +1,30 @@
 "use client"
 import React, { useState } from "react"
+import { getPost } from "../lib/serverActions"
 
 const page = () => {
-    const [inputValue, setInputValue] = useState("rien pour l'instant")
+    const [inputValue, setInputValue] = useState("")
+    const handleGetArticle = async () => {
+        const post = await getPost(1)
+        console.log(post)
+    }
 
     return (
         <div>
             <input
-                className="p-2 border border-gray-600"
+                className="p-2 mb-4 border border-gray-600"
                 type="text"
-                // value={inputValue}
+                value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
             />
             <p>Tu as écrit : {inputValue}</p>
+
+            <button
+                className="p-4 bg-green-300"
+                onClick={(e) => handleGetArticle()}
+            >
+                Récupérer un article
+            </button>
         </div>
     )
 }
